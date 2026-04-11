2263d5e89e3f0eea3940547bf02f611928357e48
Pradeep Singh <singhpradeep9802@gmail.com>
2026-04-11 23:36:34 +0530

    first commit

---
+++ basic_of_js/data_types.js
@@ -0,0 +1,30 @@
+console.log(3 + 3);
+
+console.log("Pradeep")
+
+//alert(3 + 3)
+
+
+let age = 18;
+let name = "pradeep";
+let isGradeA = 'true';
+
+// PRIMITIVE DATA TYPES:
+
+//number => 2 to the  power 53;
+//boolean => use for returning a value true or false..
+//string => " " use when value is character. and its also prefer to write in double cot.
+//bigint => but it's not much use.
+//null => standalone value..
+//undefined => when we not provide value after initialization of variable...
+//symbol => is use for uniqueness..
+
+//OBJECT
+
+
+console.log(typeof "Pradeep");
+
+// typeof () method is used for checking the what type of  variable we use is int/string/etc....
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object.
