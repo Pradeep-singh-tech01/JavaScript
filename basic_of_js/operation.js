676e9d94e78fdf8d0cb4a18d44c58f20c33641c3
Pradeep Singh <singhpradeep9802@gmail.com>
2026-04-29 09:30:00 +0530

    This this basiic js operation topic

---
+++ basic_of_js/operation.js
@@ -0,0 +1,23 @@
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// ***************  ARITHMETIC OPERATORS ************//
+//console.log(2 + 2)  => Addition of two numbers
+//console.log(2 - 2)  => Substraction of two numbers
+//console.log(2 * 2)  => multiplicatin of two numbers
+//console.log(2 / 2)  => Division of two nubers
+//console.log(2 ** 3) => Exponential of a number
+//console.log(3 % 2)  => Remainder of the the number
+
+let str1 = "Pradeep";
+let str2 = "Singh";
+
+let str3 = str1 +" "+ str2;
+//console.log(str3)
+
+
+// console.log("1" + 2)         => 12
+// console.log(1 + "2");        => 12
+// console.log("1" + 2 + 2);    => 122
+// console.log(1 + 2 + "2");    => 32
