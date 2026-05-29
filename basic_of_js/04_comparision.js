da55013cf6f52517d563f9305b61954ad3d87a54
Pradeep Singh <singhpradeep9802@gmail.com>
2026-05-30 00:28:46 +0530

    stack and heap discussion

---
+++ basic_of_js/04_comparision.js
@@ -0,0 +1,18 @@
+
+// It gives output in boolean form:-
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//console.log("2" > 1);  => Avoid to use this type of comparision because at some places it does not give the predictable answer...😊
+//console.log(2 > "1"); 
+
+
+console.log(null > 0); //=> null refers to the empty value
+console.log(null == 0);
+console.log(null >= 0);
+
