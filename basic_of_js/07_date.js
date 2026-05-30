184289895113b69dcc1ec7e54c9e715375a9dbf7
Pradeep Singh <singhpradeep9802@gmail.com>
2026-05-30 20:17:23 +0530

    Date and Time methods

---
+++ basic_of_js/07_date.js
@@ -0,0 +1,17 @@
+const myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+//let myCreatedDate = new Date(2026,4,30, 19,52,0)
+let myCreatedDate = new Date("05-30-2026") // in India we most use this formate mm/dd/yy
+// console.log(myCreatedDate.toDateString() )
+// console.log(myCreatedDate.toLocaleString())
+
+
+let date = new Date()
+console.log(date)
+console.log(date.getDay())
+console.log(date.getMonth()+1)
+console.log(date.getMinutes()) // it prints real time minutes
