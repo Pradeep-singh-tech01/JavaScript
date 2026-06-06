c8600e7802f705eb1e04572c9edd416861d5a844
Pradeep Singh <singhpradeep9802@gmail.com>
2026-06-06 11:47:43 +0530

    while and do-while loop

---
+++ 04_controlFlow_of_js/while_and_doWhile.js
@@ -0,0 +1,21 @@
+let index = 0
+while(index <= 20 ){
+    console.log(index);
+    index = index + 2
+}
+
+let myArray = ["flash" , "batman", "superman"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(myArray[arr]);
+    arr = arr + 1
+    
+    
+}
+
+let score = 1
+do {
+    console.log(`the value of score is ${score}`);
+    score++
+    
+} while (score <= 10);
