d49dc80c1ea15328182390bafbd94a70bfe808cc
Pradeep Singh <singhpradeep9802@gmail.com>
2026-06-06 11:30:05 +0530

    break and continue

--- 04_controlFlow_of_js/02_forloop.js
+++ 04_controlFlow_of_js/02_forloop.js
@@ -9,12 +9,38 @@ for(let i = 0; i < 10; i++){
 
 
 for (let i = 0; i <= 10; i++) {
-    console.log(`outer loop ${i}`);
+    //console.log(`outer loop ${i}`);
     
     for (let j = 0; j <= 10; j++) {
         //console.log(`inner loop value${j} and outer loop value${i}`);
-        console.log(i + ' * '+ j + " = " + i*j);
+        //console.log(i + ' * '+ j + " = " + i*j);
         
     }
+    
+}
+
+const myArray = ["flash", "Batman", "Supermam"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element)
+    
+}
+
+// using break keyword .....
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+       // console.log(`value 5 detected`);
+        break;
+    }
+    //console.log(`the value of i is ${index}`)
+}
+// using continue keyword......>>>>>>>>
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`value 5 is detected`);
+        continue;
+        
+    }
+    console.log(`the value of i is ${index}`)
     
 }
