15181b49d67eafcedb3c267b03314b90e5d492d2
Pradeep Singh <singhpradeep9802@gmail.com>
2026-06-09 00:44:06 +0530

    project

--- 06_project_theme_changer/color_scheme.js
+++ 06_project_theme_changer/color_scheme.js
@@ -6,7 +6,7 @@ buttons.forEach(function(button){
     button.addEventListener('click',function(e){
         console.log(e);
         console.log(e.target);
-        if(e.target.id === 'gray'){
+        if(e.target.id === 'grey'){
             body.style.backgroundColor = e.target.id;
         }
         if(e.target.id === 'white'){
