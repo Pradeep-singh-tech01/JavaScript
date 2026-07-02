0809d9cd7eb1bc7f856c81b466c6c0845b4445e0
Pradeep Singh <singhpradeep9802@gmail.com>
2026-07-02 20:50:44 +0530

    project

---
+++ 11_project_colorChanger/colorChanger.js
@@ -0,0 +1,29 @@
+// generate random color
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+const startChangingColor = function(){
+    
+    const changingBg = function(){
+        document.body.style.backgroundColor = randomColor()
+    }
+    if(!intervalId){
+        intervalId = setInterval(changingBg, 1000)
+    }
+    
+    
+}
+
+const stopChangingColor = function(){
+    clearInterval(intervalId)
+    intervalId = null;
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
